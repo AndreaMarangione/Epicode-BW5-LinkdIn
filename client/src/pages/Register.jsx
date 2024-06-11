8559aa9cdc01c36e40204f0ad7f3e80e85ce163d
= <giacomo.bartoli@me.com>
2024-06-11 18:19:52 +0200

    initial

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import LinkedinLogo from "../components/linkedinlogo/LinkedinLogo";
+
+const Register = () => {
+    return (
+        <div className={'h-100 w-400 bg-white'}>
+            <div className={'navbar container'}>
+                <div className={'navbar-brand'}>
+                    <LinkedinLogo/>
+                </div>
+            </div>
+
+            <div className={'d-flex justify-content-center align-items-center'}>
+
+            </div>
+        </div>
+    );
+};
+
+export default Register;

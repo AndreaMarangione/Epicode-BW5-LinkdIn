1f4426cebb1064f7b3ab7990c590d8e83fc6f732
= <giacomo.bartoli@me.com>
2024-06-11 17:18:52 +0200

    new strive token

--- client/src/api/experienceApi.js
+++ client/src/api/experienceApi.js
@@ -1,59 +1,59 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
-const token =
-  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjAxYzgwN2QwYjM3MTAwMWFhODk0YTEiLCJpYXQiOjE3MTUxMDA0MDAsImV4cCI6MTcxNjMxMDAwMH0.awm-TsZJX_k9vGfjTNagxOIw8wFNCB44L9IP6MjkC_k";
+const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjAxYzgwN2QwYjM3MTAwMWFhODk0YTEiLCJpYXQiOjE3MTgxMTgxNjMsImV4cCI6MTcxOTMyNzc2M30.mIKoULxMrakOoOJfdCVizV3lB9KRQZAdZQA1CYUS8i8";
+//const token = localStorage.getItem("token") && 'Bearer ' + localStorage.getItem("token")
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://striveschool-api.herokuapp.com/api/profile/",
-  prepareHeaders: (headers) => {
-    headers.set("authorization", token);
+    baseUrl: "https://striveschool-api.herokuapp.com/api/profile/",
+    prepareHeaders: (headers) => {
+        headers.set("authorization", token);
 
-    return headers;
-  },
+        return headers;
+    },
 });
 
 export const experienceApi = createApi({
-  reducerPath: "experienceApi",
-  baseQuery,
-  endpoints: (builder) => ({
-    getExperiences: builder.query({
-      query: (userId) => `${userId}/experiences`,
-      providesTags: ["Experiences"],
+    reducerPath: "experienceApi",
+    baseQuery,
+    endpoints: (builder) => ({
+        getExperiences: builder.query({
+            query: (userId) => `${userId}/experiences`,
+            providesTags: ["Experiences"],
+        }),
+        getExperience: builder.query({
+            query: ({userId, expId}) => `${userId}/experiences/${expId}`,
+            providesTags: ["Experiences"],
+        }),
+        createExperience: builder.mutation({
+            query: ({userId, newExperience}) => ({
+                url: `${userId}/experiences`,
+                method: "POST",
+                body: newExperience,
+            }),
+            invalidatesTags: ["Experiences"],
+        }),
+        deleteExperience: builder.mutation({
+            query: ({userId, expId}) => ({
+                url: `${userId}/experiences/${expId}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: ["Experiences"],
+        }),
+        editExperience: builder.mutation({
+            query: ({userId, expId, expModified}) => ({
+                url: `${userId}/experiences/${expId}`,
+                method: "PUT",
+                body: expModified
+            }),
+            invalidatesTags: ["Experiences"],
+        }),
     }),
-    getExperience: builder.query({
-      query: ({userId, expId}) => `${userId}/experiences/${expId}`,
-      providesTags: ["Experiences"],
-    }),
-    createExperience: builder.mutation({
-      query: ({ userId, newExperience }) => ({
-        url: `${userId}/experiences`,
-        method: "POST",
-        body: newExperience,
-      }),
-      invalidatesTags: ["Experiences"],
-    }),
-    deleteExperience: builder.mutation({
-      query: ({ userId, expId }) => ({
-        url: `${userId}/experiences/${expId}`,
-        method: "DELETE",
-      }),
-      invalidatesTags: ["Experiences"],
-    }),
-    editExperience: builder.mutation({
-      query: ({ userId, expId, expModified }) => ({
-        url: `${userId}/experiences/${expId}`,
-        method: "PUT",
-        body: expModified
-      }),
-      invalidatesTags: ["Experiences"],
-    }),
-  }),
 });
 
 export const {
-  useGetExperiencesQuery,
-  useCreateExperienceMutation,
-  useDeleteExperienceMutation,
-  useEditExperienceMutation,
-  useGetExperienceQuery,
+    useGetExperiencesQuery,
+    useCreateExperienceMutation,
+    useDeleteExperienceMutation,
+    useEditExperienceMutation,
+    useGetExperienceQuery,
 } = experienceApi;
--- client/src/api/profileApi.js
+++ client/src/api/profileApi.js
@@ -1,40 +1,41 @@
-  import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
-const token =
-  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjAxYzgwN2QwYjM3MTAwMWFhODk0YTEiLCJpYXQiOjE3MTUxMDA0MDAsImV4cCI6MTcxNjMxMDAwMH0.awm-TsZJX_k9vGfjTNagxOIw8wFNCB44L9IP6MjkC_k";
+const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjAxYzgwN2QwYjM3MTAwMWFhODk0YTEiLCJpYXQiOjE3MTgxMTgxNjMsImV4cCI6MTcxOTMyNzc2M30.mIKoULxMrakOoOJfdCVizV3lB9KRQZAdZQA1CYUS8i8";
+//const token = localStorage.getItem("token") && 'Bearer ' + localStorage.getItem("token")
+
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://striveschool-api.herokuapp.com/api/profile/",
-  prepareHeaders: (headers) => {
-    headers.set("authorization", token);
+    baseUrl: "https://striveschool-api.herokuapp.com/api/profile/",
+    prepareHeaders: (headers) => {
+        headers.set("authorization", token);
 
-    return headers;
-  },
+        return headers;
+    },
 });
 
 export const profileApi = createApi({
-  reducerPath: "profileApi",
-  baseQuery,
-  endpoints: builder => ({
-    getProfiles: builder.query({
-      query: () => "/",
+    reducerPath: "profileApi",
+    baseQuery,
+    endpoints: builder => ({
+        getProfiles: builder.query({
+            query: () => "/",
+        }),
+        getMyProfile: builder.query({
+            query: () => "/me",
+            providesTags: ['My-profile']
+        }),
+        getAProfile: builder.query({
+            query: (userId) => "/" + userId,
+        }),
+        ediProfile: builder.mutation({
+            query: ({mod}) => ({
+                url: '/',
+                method: 'PUT',
+                body: mod
+            }),
+            invalidatesTags: ['My-profile']
+        })
     }),
-    getMyProfile: builder.query({
-      query: () => "/me",
-      providesTags: ['My-profile']
-    }),
-    getAProfile: builder.query({
-      query: (userId) => "/" + userId,
-    }),
-    ediProfile: builder.mutation({
-      query: ({ mod}) => ({
-        url: '/',
-        method: 'PUT',
-        body: mod
-      }),
-      invalidatesTags: ['My-profile']
-    })
-  }),
 });
 
-export const { useGetProfilesQuery, useGetMyProfileQuery, useGetAProfileQuery, useEdiProfileMutation } = profileApi;
+export const {useGetProfilesQuery, useGetMyProfileQuery, useGetAProfileQuery, useEdiProfileMutation} = profileApi;

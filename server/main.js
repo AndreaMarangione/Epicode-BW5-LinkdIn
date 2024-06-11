9aedf9468a5536dd54371469de2dcaa78f1e97c4
Andrea Marangione <marangione9@gmail.com>
2024-06-11 13:08:54 +0200

    fixed .env

--- server/main.js
+++ server/main.js
@@ -8,4 +8,4 @@ connDB();
 app.use(cors());
 app.use(express.json());
 
-app.listen(process.env.PORT, () => console.log(`Server running on port${process.env.PORT}`));
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
